b72d14bec9e50fddfe37cd39868f5269ad03464c
Estebankit0 <eg221198@gmail.com>
2024-10-16 05:22:06 -0500

    First commit

---
+++ Menú/categorias.js
@@ -0,0 +1,35 @@
+const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+const boton3 = document.getElementById('boton3');
+const boton4 = document.getElementById('boton4');
+const boton5 = document.getElementById('boton5');
+const boton6 = document.getElementById('boton6');
+const boton7 = document.getElementById('boton7');
+
+boton1.addEventListener("click", function() {
+    window.location.href = 'culturaGeneral.html';
+});
+
+boton2.addEventListener("click", function() {
+    window.location.href = 'quimica.html';
+});
+
+boton3.addEventListener("click", function() {
+    window.location.href = 'biologia.html';
+});
+
+boton4.addEventListener("click", function() {
+    window.location.href = 'historia.html';
+});
+
+boton5.addEventListener("click", function() {
+    // Lógica para boton5
+});
+
+boton6.addEventListener("click", function() {
+    // Lógica para boton6
+});
+
+boton7.addEventListener("click", function() {
+    // Lógica para boton7
+});
